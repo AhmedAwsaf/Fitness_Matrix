f7de23367263a98a39bee12137df47c1fc4b12b0
AhmedAwsaf <32099454+AhmedAwsaf@users.noreply.github.com>
2025-07-19 00:28:42 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
 app.options('*', cors());
 app.use(express.json()); // Parse JSON requests
 
