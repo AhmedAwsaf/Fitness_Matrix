15e453b8d1c1a232264e765180cd7e473df5764b
AhmedAwsaf <32099454+AhmedAwsaf@users.noreply.github.com>
2025-07-19 01:47:55 +0600

    dd

--- frontend/src/context/api.jsx
+++ frontend/src/context/api.jsx
@@ -1,4 +1,4 @@
-export const backend = "http://localhost:5000/api"
+export const backend = "https://fitness-tracker-il3e.onrender.com/api"
 const BASE_URL = import.meta.env.VITE_API_URL;
 
 fetch(`${BASE_URL}/api/something`);
